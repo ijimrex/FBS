c8cd9d2a6500ed78bdcc0109945997cb676a60af
ijimrex <ijimrex@gmail.com>
2017-04-16 23:09:16 -0700

    Initial Commit

---
+++ FBSTests/FBSTests.swift
@@ -0,0 +1,36 @@
+//
+//  FBSTests.swift
+//  FBSTests
+//
+//  Created by Jimrex on 4/16/17.
+//  Copyright © 2017 Jimrex. All rights reserved.
+//
+
+import XCTest
+@testable import FBS
+
+class FBSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
